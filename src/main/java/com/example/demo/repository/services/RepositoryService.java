bb3bcfa69e32c4201ac8b030d92992fe0135a887
Łukasz Bojeś <lukasz.bojes@coig.pl>
2018-08-28 13:56:41 +0200

    UPDATE4

--- src/main/java/com/example/demo/repository/services/RepositoryService.java
+++ src/main/java/com/example/demo/repository/services/RepositoryService.java
@@ -3,8 +3,6 @@ package com.example.demo.repository.services;
 import com.example.demo.repository.dto.RepositoryDto;
 import com.example.demo.repository.dto.ResponseDto;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -15,8 +13,8 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class RepositoryService {
-    private static final Logger logger = LoggerFactory.getLogger(RepositoryService.class);
     private final RestTemplate restTemplate;
+
 
     public RepositoryService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.errorHandler(new DefaultResponseErrorHandler()).build();
@@ -24,9 +22,8 @@ public class RepositoryService {
 
     @Async
     public CompletableFuture<ResponseDto> findRepository(String url) {
-        logger.info("Looking up " + url);
         ModelMapper modelMapper = new ModelMapper();
-        RepositoryDto repositoryDto = restTemplate.getForObject(url, RepositoryDto.class);
-        return CompletableFuture.completedFuture(repositoryDto).thenApply(result -> modelMapper.map(result, ResponseDto.class));
+        return CompletableFuture.completedFuture(restTemplate.getForObject(url, RepositoryDto.class))
+                .thenApply(result -> modelMapper.map(result, ResponseDto.class));
     }
 }
